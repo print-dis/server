58379fa7e5e28f0fd923d105ad4fdae2eee3414a
Braxton Schafer <braxton@cmdcentral.xyz>
2023-06-06 10:11:16 -0500

    style: reformat (don't use jetbrains kids)

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,9 @@
-pub  mod  db;
-pub  mod  filament;
-pub  mod filament_type;
-pub  mod  filament_filament_type;
-pub  mod print_request;
-pub  mod  printer;
-pub  mod  user;
+pub mod db;
+pub mod filament;
+pub mod filament_filament_type;
+pub mod filament_type;
+pub mod print_request;
+pub mod printer;
+pub mod user;
 
-pub  mod  models{}
+pub mod models {}
